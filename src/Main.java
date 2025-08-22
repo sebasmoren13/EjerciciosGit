878e4f81b98ac074759e22de4e10444bc94bfa44
sebasmoren13 <sebasmoren13@gmail.com>
2025-08-22 17:11:28 -0500

    PRIMEROS DATOS

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+        System.out.println("SEBASTIAN ANDRES PRIETO MORENO");
+        System.out.println("ESTUDIANTE DE LA CUNDINAMARCA");
+
+
+    }
+}
